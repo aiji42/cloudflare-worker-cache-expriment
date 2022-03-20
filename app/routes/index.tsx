9b1dede2e7320d8a541bf3f905a52d713eab1107
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-03-20 23:24:29 +0900

    experient cache

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,33 @@
+import { HeadersFunction, LoaderFunction, useLoaderData } from "remix";
+
+export const loader: LoaderFunction = async ({
+  request,
+  context: { event, cache },
+}) => {
+  const cachedRes = await cache.match(request);
+  if (cachedRes) return cachedRes;
+
+  const now = new Date().toString();
+  const response = new Response(now, {
+    headers: {
+      "Cache-Control": "max-age=10",
+    },
+  });
+
+  event.waitUntil(cache.put(request, response.clone()));
+  return response;
+};
+
+export const header: HeadersFunction = ({ loaderHeaders }) => {
+  return loaderHeaders;
+};
+
 export default function Index() {
+  const data = useLoaderData();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <span>{data}</span>
     </div>
   );
 }
